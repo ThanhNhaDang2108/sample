25187ab2c2197472242fc3f2280fd61a67662f82
yinepic <anhquangepic@gmail.com>
2020-07-07 16:17:08 +0700

    Create server.py
    create server using asyncio

---
+++ server.py
@@ -0,0 +1,30 @@
+import asyncio
+
+async def handle_echo(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+    print("Received %r from %r" % (message, addr))
+
+    new_message = "Send from server to client"
+    print("Send: %r" % new_message)
+    writer.write(new_message.encode())
+    await writer.drain()
+
+    print("Close the client socket")
+    writer.close()
+
+loop = asyncio.get_event_loop()
+coro = asyncio.start_server(handle_echo, '127.0.0.1', 5687, loop=loop)
+server = loop.run_until_complete(coro)
+
+print('Serving on {}'.format(server.sockets[0].getsockname()))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
+# Close the server
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
